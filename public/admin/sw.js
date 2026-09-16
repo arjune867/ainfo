8ce1fa42a85c08c5381b0f891b5e17ebe2ebca81
applikasiku <amelnewsid@gmail.com>
2026-09-16 08:58:01 +0700

    chore(pwa): bump admin cache for V14.2

--- public/admin/sw.js
+++ public/admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ainfo-admin-v13';
+const CACHE='ainfo-admin-v14-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','../AINFO.png','../icon-192.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ainfo-admin-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
