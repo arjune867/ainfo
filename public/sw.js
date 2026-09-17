2a8bd2e7b39e82b67fff1ea3e1f78b13601edd8e
applikasiku <amelnewsid@gmail.com>
2026-09-17 12:56:26 +0700

    chore(pwa): refresh auto-hide video cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ainfo-v14-17-videoengine-4';
+const CACHE='ainfo-v14-17-videoengine-5';
 const CORE=['./','./index.html','./manifest.webmanifest','./AINFO.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./offline.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
