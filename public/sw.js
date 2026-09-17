4757f6ee1b2fbf824aa4a3b26b68b6a76553013f
applikasiku <amelnewsid@gmail.com>
2026-09-17 10:45:00 +0700

    chore(v14.14): refresh PWA cache after engagement fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ainfo-v14-13-shopfix-1';
+const CACHE='ainfo-v14-14-engagementfix-1';
 const CORE=['./','./index.html','./manifest.webmanifest','./AINFO.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./offline.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
